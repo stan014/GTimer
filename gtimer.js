d7bf14f53c2e9807647003a49000c5c1c9957db8
stan014 <stan014@gmail.com>
2015-05-10 00:14:00 +0800

    GTimer

---
+++ gtimer.js
@@ -0,0 +1,108 @@
+﻿var GTimer = function () {
+    var unit = 1000;
+    var isStart = false;
+    var jobs = [];
+    var timer;
+    function Job(n, i, f) {
+        var countdown = i;
+        var enable = false;
+        this.id = n;
+        this.interval = i;
+        this.Work = f;
+        this.step = function () {
+            if (enable) {
+                countdown -= unit;
+                if (countdown <= 0) {
+                    countdown = this.interval;
+                    this.Work();
+                }
+            }
+        }
+        this.start = function () {
+            if (!enable) {
+                countdown = this.interval;
+                enable = true;
+
+            }
+        }
+        this.stop = function () {
+            enable = false;
+        }
+        this.toggle = function (e) {
+            if (e) {
+                this.start();
+            } else {
+                this.stop();
+            }
+        }
+        this.getDesc = function () {
+            return { id: this.id, interval: this.interval };
+        }
+    }
+
+    function find(id) {
+        for (var i = 0; i < jobs.length; i++) {
+            if (jobs[i].id == id) {
+                return { i: i, item: jobs[i] };
+            }
+        }
+    }
+
+    function toggle(id, enable) {
+        if (id) {
+            var exist = find(id);
+            if (exist) {
+                exist.item.toggle(enable);
+            }
+        } else {
+            for (var i = 0; i < jobs.length; i++) {
+                jobs[i].toggle(enable);
+            }
+        }
+        if (enable && !isStart) {
+            isStart = true;
+            timer = window.setInterval(function () {
+                for (i = 0; i < jobs.length; i++) {
+                    jobs[i].step();
+                }
+            }, unit);
+        }
+    }
+
+    return {
+        add: function (id, interval, hanlder) {
+            var j = new Job(id, interval, hanlder);
+            var exist = find(id);
+            if (exist) {
+                jobs[exist.i] = j;
+            } else {
+                jobs.push(j);
+            }
+        },
+        remove: function (id) {
+            var exist = find(id);
+            if (exist) {
+                jobs.splice(exist.i, 1);
+            }
+        },
+        getJobs: function () {
+            var list = [];
+            for (var i = 0; i < jobs.length; i++) {
+                list.push(jobs[i].getDesc());
+            }
+            return list;
+        },
+        start: function (id) {
+            toggle(id, true);
+        },
+        stop: function (id) {
+            toggle(id, false);
+        },
+        destory: function () {
+            jobs = [];
+            if (isStart) {
+                clearInterval(timer);
+            }
+        }
+    }
+}();
